1f1858858b687b071db78f998706fa71de204e69
marsel8801 <marsel8801@gmail.com>
2026-06-15 19:01:21 +0600

    FINISH SALE

--- src/components/Layout/SALE/SALE.jsx
+++ src/components/Layout/SALE/SALE.jsx
@@ -12,14 +12,36 @@ function SALE() {
               <h2>Lip In 10% ofl on white lind SPyfils</h2>
             </div>
             <div className="sale-photo">
-                <img src="./sale1.svg" alt="logo" />
+              <img src="./sale1.svg" alt="logo" />
+            </div>
+          </div>
+        </div>
+
+        <div className="filter">
+          <h1>FALTER</h1>
+          <div className="fil-text">
+            <h2>Filter</h2>
+            <select>
+              <option value="popular">Popular</option>
+              <option value="csocs">Csocs</option>
+              <option value="shlepka">shlepka</option>
+              <option value="argen gay">argen gay</option>
+            </select>
+            <h4>100 dpmg</h4>
+          </div>
+          <div className="filter-img">
+            <img src="./saleLogo1.svg" alt="logo" />
+            <div className="fil-com">
+              <h1>New Balance </h1>
+              <div className="fil-bn">
+                <h2>34.99$ - </h2>
+                <h3>sale up 20%</h3>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </section>
-    
-
   );
 }
 
